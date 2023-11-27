cb7761ae3f73995f111801a95eb4ceaacdaefee9
Programmer Develops <74005248+Programmer-Develops@users.noreply.github.com>
2023-11-27 11:35:04 +0530

    Add files via upload

--- play-on-click.js
+++ play-on-click.js
@@ -1,33 +1,36 @@
 AFRAME.registerComponent("play-on-click", {
- //Add code here
   schema: {
-    isPlaying:{
-        default:false,
-        type:"boolean",
-    }
+    isPlaying: { type: "boolean", default: false }
   },
 
-  init: function () {
-    this.videoelement = this.el.getAttribute("material").src
-    this.onClick = this.onClick.bind(this)
+  init: function() {
+    this.videoEl = this.el.getAttribute("material").src;
+    this.onClick = this.onClick.bind(this);    
   },
+
   play: function() {
-    window.addEventListener("click", this.onClick)
-
+    window.addEventListener("click", this.onClick);
   },
-  onClick: function() {
-    if ( !this.videoelement) {
-        return
+  onClick: function(evt) {
+    if (!this.videoEl) {
+      return;
     }
-    var isPlaying = this.el.getAttribute("play-on-click").isPlaying
-    this.el.object3D.visible = true
+
+    var isPlaying = this.el.getAttribute("play-on-click").isPlaying;
+
+    this.el.object3D.visible = true;
+
     if (!isPlaying) {
-        this.el.setAttribute("play-on-click",{isPlaying:true})
-        this.videoelement.play()
-     }
-    else{
-        this.el.setAttribute("play-on-click",{isPlaying:false})
-        this.videoelement.pause()
+      this.el.setAttribute("play-on-click", {
+        isPlaying: true
+      });
+      this.videoEl.play();
+    } else {
+      this.el.setAttribute("play-on-click", {
+        isPlaying: false
+      });
+      this.videoEl.pause();
     }
   }
 });
+
